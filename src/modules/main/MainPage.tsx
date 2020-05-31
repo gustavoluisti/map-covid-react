2f3217c637130342b58a076ae23dd52e5779c95b
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-05-31 16:15:19 -0300

    Estilizando aplicação

--- src/modules/main/MainPage.tsx
+++ src/modules/main/MainPage.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styles from "./MainPage.module.css";
+import Mapbox from "./components/Map/Mapbox";
 
 export default function MainPage() {
   return (
     <div className={styles.container}>
-      <div className={styles.mapContainer}>Mapa</div>
+      <div className={styles.mapContainer}>
+        <Mapbox />
+      </div>
       <div className={styles.chartContainer}>Gráfico</div>
       <div className={styles.rankingContainer}>Ranking</div>
       <div className={styles.playerContainer}>Player</div>
---
+++ src/modules/main/components/Map/Mapbox.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useRef, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import styles from "./Mapbox.module.css";
+
+mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
+const centerLocation: mapboxgl.LngLatLike = [5, 34];
+
+export default function MapBox() {
+  const [initialized, setInitialized] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const mapRef = useRef<mapboxgl.Map | null>(null);
+
+  useEffect(() => {
+    if (containerRef.current === null) return;
+
+    let mounted = true;
+    setInitialized(false);
+
+    mapRef.current = new mapboxgl.Map({
+      container: containerRef.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: centerLocation,
+      zoom: 1,
+    });
+
+    mapRef.current.on("load", () => {
+      if (mounted === false) return;
+      setInitialized(true);
+    });
+
+    return () => {
+      mounted = false;
+      mapRef.current!.remove();
+    };
+  }, [mapRef, containerRef]);
+
+  return <div className={styles.container} ref={containerRef}></div>;
+}
